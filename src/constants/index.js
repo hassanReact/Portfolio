a564bee5fca7a25503dfc39144d6e1720d18a48a
Hassan Kashif <hassank1751@gmail.com>
2024-11-27 01:13:31 -0800

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Ali Khan',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvf244QoldfFIeyhysx5kQ3n6PptTBcfSQA&s',
+    review:
+      'Working with Hassan was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Ahmed Raza',
+    position: 'Founder of TechGear Shop',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvf244QoldfFIeyhysx5kQ3n6PptTBcfSQA&s',
+    review:
+      'Hassan’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'Usman Javed',
+    position: 'Project Manager at UrbanTech',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvf244QoldfFIeyhysx5kQ3n6PptTBcfSQA&s',
+    review:
+      'I can’t say enough good things about Hassan. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Fatima Noor',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvf244QoldfFIeyhysx5kQ3n6PptTBcfSQA&s',
+    review:
+      'Hassan was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend development are top-notch.',
+  },
+];
+export const myProjects = [
+  {
+    title: 'React - Single Page Websites',
+    desc: 'React is a powerful JavaScript library for building dynamic user interfaces. With its component-based architecture and efficient virtual DOM, it enables developers to create fast and interactive web applications effortlessly.',
+    subdesc:
+      'Built as a unique platform with React, Tailwind CSS, Gsap, Three.js, and Vite, modern websites created with this stack are designed for optimal performance and scalability. This combination empowers developers to build visually appealing, interactive, and highly efficient web experiences tailored for diverse user needs.',
+    href: '/',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Dynamic E-Commerce Platform',
+    desc: 'A robust e-commerce platform featuring smooth navigation, secure payment gateways, and intuitive user experiences. Tailored for scalability, this solution boosts customer engagement and drives online sales.',
+    subdesc:
+      'Developed with React, GSAP animations, and Tailwind CSS, this e-commerce platform ensures lightning-fast performance and a visually stunning interface that captivates users.',
+    href:'hassanportfolio.infinityfreeapp.com',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/ecommerce-logo.png',
+    logoStyle: {
+      backgroundColor: '#1E2A38',
+      border: '0.2px solid #2C3F52',
+      boxShadow: '0px 0px 60px 0px #1F678A4D',
+    },
+    spotlight: '/assets/ecommerce-spotlight.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Interactive Animation Website',
+    desc: 'An immersive website experience featuring advanced animations and interactive elements, perfect for showcasing creative portfolios and storytelling.',
+    subdesc:
+      'Powered by GSAP and Three.js, this project blends high-quality animations with seamless user interaction, creating a standout visual journey.',
+    href: '/',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/animation-logo.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/animation-spotlight.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Recipe Finder - API-Driven Website',
+    desc: 'A recipe search website powered by real-time API integration, offering a vast collection of recipes with filters for dietary preferences, cuisine types, and ingredients.',
+    subdesc:
+      'Built with React, REST APIs, and Tailwind CSS, this website delivers a responsive and intuitive interface for users to discover and save their favorite recipes.',
+    href: 'https://foods-counter-recipe.surge.sh/',
+    texture: '/textures/project/recipe.mp4',
+    logo: '/assets/recipe-logo.png',
+    logoStyle: {
+      backgroundColor: '#FFFAF0',
+      border: '0.2px solid #FFAB73',
+      boxShadow: '0px 0px 60px 0px #FF98524D',
+    },
+    spotlight: '/assets/recipe-spotlight.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Sibex Systems',
+    pos: 'React JS Developer',
+    duration: 'May 2024 - Present',
+    title: "Developed and maintained responsive web applications using React.js, leveraging React Hooks, Redux Toolkit, and Context API for efficient state management. Designed and implemented fully responsive layouts using Tailwind CSS and Bootstrap, and enhanced user experiences with animations via GSAP and 3D elements with Three.js.",
+    icon: '/assets/react.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'The Forman College',
+    pos: 'Student',
+    duration: '2022 - 2024',
+    title: "Built a strong foundation in mathematics and physics, which set the stage for my journey into technology and software development.",
+    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTllB-N2-Ae8c_Pa5_Rszl1DPBej-nqIqV51Q&s',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'The Virtual University',
+    pos: 'B.S. Computer Science Student',
+    duration: 'Sep 2024 - Present',
+    title: "Currently pursuing a degree in Computer Science, focusing on programming, algorithms, and software development, while deepening my expertise in React.js and frontend development.",
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/VU_Logo.png/800px-VU_Logo.png',
+    animation: 'salute',
+  },
+];
